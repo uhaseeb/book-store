f6bdbcb5c9c0d8eb1d4304cfaaa3ccfb5c01062a
uhaseeb <uhaseeb731@gmail.com>
2022-08-12 10:44:16 +0500

    data relations done

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,12 +4,48 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=3)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.city} ({self.postal_code})"
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+
 class Books(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country, null=False, related_name="books")
 
     def get_absolute_url(self):
         return reverse("book_details", args=[self.slug])
@@ -20,3 +56,6 @@ class Books(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.author})"
+
+    class Meta:
+        verbose_name_plural = "Books"
